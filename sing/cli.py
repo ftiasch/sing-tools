fb688e966827100596e929c90e63b96e1b1a1cc9
Xiaoxu Guo <ftiasch0@gmail.com>
2023-12-27 11:55:02 +0800

    finished script

--- sing/cli.py
+++ sing/cli.py
@@ -27,7 +27,7 @@ def down():
 
 
 def okgg_filter(name: str, _: dict) -> bool:
-    if "HongKong" in name:
+    if "AI" in name or "ChatGPT" in name:
         return True
     return False
 
--- sing/gen.sh
+++ sing/gen.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -o errexit
-
+cd "$(dirname "${BASH_SOURCE[0]}")"
 . ../.env/bin/activate
+if [[ "$1" == "down" ]]; then
+	python cli.py down
+fi
 sudo systemctl stop sing-box@custom
 python cli.py gen
 sudo sing-box merge /etc/sing-box/custom.json -c config.template.json -c run/config.json
