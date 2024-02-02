4c5a684729883b0140fd9e02eeee59d19426d68b
Xiaoxu Guo <ftiasch0@gmail.com>
2024-02-02 10:42:22 +0800

    added

--- cli.py
+++ cli.py
@@ -25,9 +25,12 @@ def down():
 
 
 def okgg_filter(name: str, _: dict) -> bool:
-    if "IPv6" in name:
+    for t in ("IPv6", "Test", "应急"):
+        if t in name:
+            return False
+    if "AI" not in name.split("-"):
         return False
-    for loc in ("日本", "韩国", "新加坡", "Singapore", "香港", "HongKong"):
+    for loc in ("日本", "韩国", "新加坡", "Singapore", "Tailand", "香港", "HongKong"):
         if loc in name:
             return True
     return False
@@ -43,7 +46,7 @@ def ww_filter(name: str, _: dict) -> bool:
 def select(nameserver: Optional[str] = None) -> Parser:
     parser = Parser(nameserver)
     parser.parse("okgg", okgg_filter)
-    parser.parse("ww", ww_filter)
+    # parser.parse("ww", ww_filter)
     return parser
 
 
