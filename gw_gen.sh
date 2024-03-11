d6612298c8f7e9e553285d940340078c246c53eb
Xiaoxu Guo <ftiasch0@gmail.com>
2024-03-11 11:34:53 +0800

    added gw

---
+++ gw_gen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -o errexit
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+. .env/bin/activate
+
+if [[ "$1" == "down" ]]; then
+	shift
+	python cli.py down
+fi
+
+# python cli.py gw_gen $@
+# sing-box check -c run/config.json
+
+rsync run/config.json marf-gw:/etc/sing-box
+
+for d in yacd sing-geoip sing-geosite; do
+	rsync -arz /usr/share/$d marf-gw:/usr/share
+done
