55d9b507a93036d2e8f68f31d9650626261a5eed
Xiaoxu Guo <shimo11370@proton.me>
2024-12-20 10:54:33 +0800

    updated gen

--- gen.sh
+++ gen.sh
@@ -13,6 +13,9 @@ rsync run/config.json marf-gw:/etc/sing-box
 rsync -arz /usr/share/yacd-meta/ marf-gw:/usr/share/yacd-meta/
 ssh marf-gw service sing-box restart
 
+rsync run/config.json marf-sh:/etc/sing-box
+ssh marf-sh service sing-box restart
+
 prefix=/home/ftiasch/Sync/sing
 sed 's@/usr/share@.@g' run/config.json >$prefix/config.json
 # rsync -arz /usr/share/sing-box/sing-geosite-rule-set/ $prefix/sing-geosite/
